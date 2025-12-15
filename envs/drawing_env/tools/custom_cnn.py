a5b667f80c315bda02a4db88e382ee6cb1ee2898
Dio990521 <dcz990521@gmail.com>
2025-12-15 22:14:55 +0900

    grey update

--- envs/drawing_env/tools/custom_cnn.py
+++ envs/drawing_env/tools/custom_cnn.py
@@ -9,18 +9,18 @@ class CustomCnnExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: spaces.Space, features_dim: int = 128, padding=True):
         super().__init__(observation_space, features_dim)
 
-        self.use_jump_counter = isinstance(observation_space, spaces.Dict)
+        self.use_dist_val = isinstance(observation_space, spaces.Dict)
 
-        if self.use_jump_counter:
+        if self.use_dist_val:
             image_space = observation_space.spaces["image"]
-            jump_space = observation_space.spaces["jump_counter"]
+            dist_val = observation_space.spaces["dist_val"]
 
             n_input_channels = image_space.shape[0]
-            jump_dim = jump_space.shape[0]
+            dist_val_dim = dist_val.shape[0]
         else:
             image_space = observation_space
             n_input_channels = image_space.shape[0]
-            jump_dim = 0
+            dist_val_dim = 0
 
         self.cnn = nn.Sequential(
             nn.Conv2d(n_input_channels, 32, kernel_size=3, stride=1, padding=int(padding)),
@@ -37,15 +37,15 @@ class CustomCnnExtractor(BaseFeaturesExtractor):
             n_flatten = self.cnn(sample_input).shape[1]
 
         self.linear = nn.Sequential(
-            nn.Linear(n_flatten + jump_dim, features_dim),
+            nn.Linear(n_flatten + dist_val_dim, features_dim),
             nn.ReLU()
         )
 
     def forward(self, observations) -> torch.Tensor:
-        if self.use_jump_counter:
+        if self.use_dist_val:
             img_tensor = observations["image"]
-            jump_tensor = observations["jump_counter"]
+            dist_val = observations["dist_val"]
             cnn_features = self.cnn(img_tensor)
-            combined_features = torch.cat((cnn_features, jump_tensor), dim=1)
+            combined_features = torch.cat((cnn_features, dist_val), dim=1)
             return self.linear(combined_features)
         return self.linear(self.cnn(observations))
